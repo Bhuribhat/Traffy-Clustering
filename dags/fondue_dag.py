7b487a73479a3a93e34cdb1e21d47769125a5848
Bhuribhat <Bhuribhat@gmail.com>
2023-05-09 22:17:33 +0700

    add email on fail

--- dags/fondue_dag.py
+++ dags/fondue_dag.py
@@ -8,6 +8,8 @@ default_args = {
     'owner': 'pras',
     # 'depends_on_past': False,
     # 'start_date': datetime(2023, 5, 9),
+    'email_on_failure': False,
+    'email': ['Bhuribhat@gmail.com'],
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
