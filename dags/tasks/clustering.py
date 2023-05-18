b2a5b13c2d88f5b1aa52ee4c002b635baff80f6e
bbompk <panithiv@gmail.com>
2023-05-18 15:11:36 +0700

    utilize mlflow

--- dags/tasks/clustering.py
+++ dags/tasks/clustering.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 plt.style.use('ggplot')
+import datetime
 
 # MLflow
 import mlflow
@@ -28,27 +29,42 @@ def kmean_cluster(ti, **context) :
 
     cluster_data = scaler.fit_transform(x)
 
-    n_clusters = 6
+    #create a new experiment
+    now = datetime.datetime.now()
+    experiment_name = 'Airflow DAG triggered: {}'.format(now)
+    try:
+        exp_id = mlflow.create_experiment(name=experiment_name)
+    except Exception as e:
+        exp_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
+        mlflow.delete_experiment(exp_id)
+        exp_id = mlflow.create_experiment(name=experiment_name)
 
-    kmeans = KMeans(n_clusters=n_clusters, random_state=2020)
-    kmeans.fit(cluster_data)
+    max_cluster = 30
+    labels = [None]
 
-    summation = sum(np.min(cdist(cluster_data, kmeans.cluster_centers_, 'euclidean'), axis=1))
-    meandistortion = summation / cluster_data.shape[0]
-    
+    # Run Experiment of this DAG trigger
+    for k in range(1, max_cluster):
+        with mlflow.start_run(experiment_id=exp_id, run_name=f"n_clusters = {k}") as run:
+            kmeans = KMeans(n_clusters=k, random_state=2020)
+            kmeans.fit(cluster_data)
+            labels.append(kmeans.labels_)
 
-    df['cluster_label'] = kmeans.labels_
+            summation = sum(np.min(cdist(cluster_data, kmeans.cluster_centers_, 'euclidean'), axis=1))
+            meandistortion = summation / cluster_data.shape[0]
+
+            mlflow.log_param('n_clusters', k)
+            mlflow.log_artifact('/opt/airflow/data/cleaned_fondue.csv')
+            mlflow.log_metric('inertia', kmeans.inertia_)
+            mlflow.log_metric('meandistortion', meandistortion)
+
+            mlflow.sklearn.log_model(kmeans, 'model')
+
+    chosen_k = 6
+
+    # use the label from the best model
+    df['cluster_label'] = labels[chosen_k]
     df.name = 'clustered_fondue'
     save_files([df])
-
-    # Log mlflow
-    mlflow.set_experiment('fondue')
-    with mlflow.start_run() as run:
-        mlflow.log_param('n_clusters', n_clusters)
-        mlflow.log_metric('inertia', kmeans.inertia_)
-        mlflow.log_metric('meandistortions', meandistortion)
-
-        mlflow.sklearn.log_model(kmeans, 'model')
 
         
 if __name__ == '__main__':
